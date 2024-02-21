31fa4487536d95d34379f179aca1ccd978f274aa
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2024-02-21 16:09:18 +0100

    fixed responsive with next/previous buton to scrol

---
+++ js/scroll.js
@@ -0,0 +1,27 @@
+// Supposons que 'scrollButton' est la classe de votre bouton
+var scrollDownButtons = document.getElementsByClassName('scrollDownButton');
+
+// Comme getElementsByClassName retourne une collection, vous itérer sur chaque élément
+Array.from(scrollDownButtons).forEach(function(button) {
+  button.addEventListener('click', function() {
+    // Calcule 100% de la hauteur de la fenêtre du navigateur
+    const height = window.innerHeight;
+  
+    // Fait défiler la page de 100vh
+    window.scrollBy({ top: height, left: 0, behavior: 'smooth' });
+  });
+});
+
+// Def le button
+var scrollUpButtons = document.getElementsByClassName('scrollUpButton');
+
+// Comme getElementsByClassName retourne une collection, itérer sur chaque élément
+Array.from(scrollUpButtons).forEach(function(button) {
+  button.addEventListener('click', function() {
+    // Calcule 100% de la hauteur de la fenêtre du navigateur
+    const height = window.innerHeight;
+  
+    // Fait défiler la page de 100vh
+    window.scrollBy({ top: -height, left: 0, behavior: 'smooth' });
+  });
+});
